f451b9f8c6d33a90ae3873eae3a887c1fd3f182f
LaoYutang <539870188@qq.com>
2023-01-26 10:14:50 +0800

    feat(custom): init

--- .cz-config.js
+++ .cz-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   types: [
-    { value: 'feature', name: 'feature:  增加新功能' },
+    { value: 'feat', name: 'feat:  增加新功能' },
     { value: 'bug', name: 'bug:      测试反馈bug列表中的bug号' },
     { value: 'fix', name: 'fix:      修复bug' },
     { value: 'ui', name: 'ui:       更新UI' },
@@ -26,9 +26,7 @@ module.exports = {
     type: '请选择提交类型:',
     customScope: '请输入您修改的范围(可选):',
     subject: '请简要描述提交 message (必填):',
-    body: '请输入详细描述(可选，待优化去除，跳过即可):',
-    footer: '请输入要关闭的issue(待优化去除，跳过即可):',
-    confirmCommit: '确认使用以上信息提交？(y/n/e/h)',
+    confirmCommit: '确认使用以上信息提交？(y/n)',
   },
   allowCustomScopes: true,
   skipQuestions: ['body', 'footer'],
